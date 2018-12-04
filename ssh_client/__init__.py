ea0463535030564f986eef6cd87796144c8e1147
Brandon S <atomicbcs@gmail.com>
2018-12-03 20:43:52 -0500

    Got mutiple things workong

--- ssh_client/__init__.py
+++ ssh_client/__init__.py
@@ -1,17 +1,33 @@
+from threading import Thread
+import time, socket
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 import paramiko
+from paramiko.py3compat import u
 
 socketio = SocketIO()
-hostname = ""
-username = ""
-password = ""
-client = paramiko.Transport((hostname, 22))
-client.connect(username=username, password=password)
-session = client.open_channel(kind='session')
-session.get_pty()
-session.invoke_shell()
-session.settimeout(0.01)
+clients = {}
+
+def threaded_function(args):
+    print("Context",args.app_context())
+    with args.test_request_context():
+        while(True):
+            for c in clients:
+                client = clients[c]
+                session = client[0]
+                sid = client[1]
+                # print("serving client",sid)
+                try:
+                    x = u(session.recv(4096))
+                    if len(x) == 0:
+                        print("DONE")
+                        break
+                    emit('response', {'data': x}, room=sid, namespace='/')
+                except socket.timeout:
+                    break
+                print("sleeping")
+            time.sleep(0.01)
+
 
 def create_app():
     app = Flask(__name__, template_folder="views/templates", static_url_path='/static')
@@ -22,7 +38,8 @@ def create_app():
     from ssh_client.views import ssh_client
     
 
-    
+    # thread = Thread(target = threaded_function, args = (app,))
+    # thread.start()
     
 
     app.register_blueprint(ssh_client.page, url_prefix='/')
--- ssh_client/views/ssh_client.py
+++ ssh_client/views/ssh_client.py
@@ -1,14 +1,16 @@
+import time
+from threading import Thread
 from flask import Blueprint, render_template, abort, flash
+from flask import copy_current_request_context
 from flask import request
 from flask_socketio import send, emit
-from .. import socketio, client, session
+from .. import socketio, clients
 import sys, traceback, socket
 from paramiko.py3compat import u
 import paramiko
 import select
 
 page = Blueprint('main', __name__, template_folder='templates')
-
 
 
 
@@ -24,52 +26,43 @@ def handle_my_custom_event2(resp):
     # session = client.open_channel(kind='session')
     try:
         key = resp['key']
-        print("Sending ",key)
-
-        # print("Sending ",chr(resp['key']))
-        # char = chr(resp['key'])
-        # if key == 37:
-        #     char = b'\x1b[D'
-        # elif key == 38:
-        #     char = b'\x1b[A'
-        # elif key == 39:
-        #     char = b'\x1b[C'
-        # elif key == 40:
-        #     char = b'\x1b[B'
+        sid = request.sid
+        sess_key = resp['sess_key']
+        if sess_key == -1:
+            return
+        session = clients[sid]
         session.send(key)
-    except socket.timeout:
-        pass
+    except KeyError:
+        return
     print("hmm")
-    while True:
-        try:
-            x = u(session.recv(4096))
-            if len(x) == 0:
-                print("DONE")
-                break
-            emit('response', {'data': x})
-        except socket.timeout:
-            break
-    stdout_data = ''.join(map(chr,stdout_data))
-    # emit('response', {'data': stdout_data})
-
-# @socketio.on('command')
-# def handle_my_custom_event2(resp):
-#     stdout_data = []
-#     stderr_data = []
-#     session = client.open_channel(kind='session')
-#     print(resp)
-#     session.exec_command(resp['command'])
-#     while True:
-#         if session.recv_ready():
-#             stdout_data += session.recv(4096)
-#         if session.recv_stderr_ready():
-#             stderr_data += session.recv_stderr(4096)
-#         if session.exit_status_ready():
-#             break
-#     stdout_data = ''.join(map(chr,stdout_data))
-#     print(stdout_data)
-#     emit('response', {'data': stdout_data})
 
 @socketio.on('login')
 def ssh_login(info):
-    print(info)
+    @copy_current_request_context
+    def background_thread():
+        """Example of how to send server generated events to clients."""
+        count = 0
+        while True:
+            r, w, e = select.select([session], [], [])
+            if session in r:
+                try:
+                    x = u(session.recv(1024))
+                    if len(x) == 0:
+                        break
+                    emit('response', {'data': x}, room=sid, namespace='/')
+                    socketio.sleep(0)
+                except socket.timeout:
+                    pass
+    sid = request.sid
+    hostname = info['hostname']
+    username = info['username']
+    password = info['password']
+    client = paramiko.Transport((hostname, 22))
+    client.connect(username=username, password=password)
+    session = client.open_channel(kind='session')
+    session.get_pty()
+    session.invoke_shell()
+    session.settimeout(0.00)
+    clients[sid] = (session)
+    thread = Thread(target = background_thread)
+    thread.start()
