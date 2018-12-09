de32f5af0dcb6f83153ce3cb7bac4552f9e3a3d0
Brandon S <atomicbcs@gmail.com>
2018-12-09 16:51:06 -0500

    fixed bugs

--- ssh_client/__init__.py
+++ ssh_client/__init__.py
@@ -8,26 +8,6 @@ from paramiko.py3compat import u
 socketio = SocketIO()
 clients = {}
 
-def threaded_function(args):
-    print("Context",args.app_context())
-    with args.test_request_context():
-        while(True):
-            for c in clients:
-                client = clients[c]
-                session = client[0]
-                sid = client[1]
-                # print("serving client",sid)
-                try:
-                    x = u(session.recv(4096))
-                    if len(x) == 0:
-                        print("DONE")
-                        break
-                    emit('response', {'data': x}, room=sid, namespace='/')
-                except socket.timeout:
-                    break
-                print("sleeping")
-            time.sleep(0.01)
-
 
 def create_app():
     app = Flask(__name__, template_folder="views/templates", static_url_path='/static')
@@ -36,11 +16,6 @@ def create_app():
     # Register all views after here
     # =======================
     from ssh_client.views import ssh_client
-    
-
-    # thread = Thread(target = threaded_function, args = (app,))
-    # thread.start()
-    
 
     app.register_blueprint(ssh_client.page, url_prefix='/')
     socketio.init_app(app)
--- ssh_client/views/ssh_client.py
+++ ssh_client/views/ssh_client.py
@@ -19,6 +19,25 @@ def show_client():
     # flash("Grid loaded", 'success')
     return render_template('index.html')
 
+
+@socketio.on('resize')
+def handle_my_custom_event3(resp):
+    try:
+        sid = request.sid
+        if sid not in clients:
+            return
+        session = clients[sid][0]
+        if 'rows' in resp:
+            rows = int(resp['rows'])
+            cols = int(resp['cols'])
+            if rows != clients[sid][1][1] or cols != clients[sid][1][0]:
+                clients[sid][1][0] = cols
+                clients[sid][1][1] = rows
+                session.resize_pty(height = rows, width = cols) 
+    except Exception as e:
+        print(e)
+        return
+
 @socketio.on('keypress')
 def handle_my_custom_event2(resp):
     stdout_data = []
@@ -30,9 +49,23 @@ def handle_my_custom_event2(resp):
         sess_key = resp['sess_key']
         if sess_key == -1:
             return
-        session = clients[sid]
+        session = clients[sid][0]
+        if 'rows' in resp:
+            rows = int(resp['rows'])
+            cols = int(resp['cols'])
+            if rows != clients[sid][1][1] or cols != clients[sid][1][0]:
+                clients[sid][1][0] = cols
+                clients[sid][1][1] = rows
+                session.resize_pty(height = rows, width = cols) 
         session.send(key)
     except KeyError:
+        return
+    except OSError:
+        del clients[sid]
+        x = "Disconnected from server\r\n\r\n"
+        emit('response', {'data': x, 'clear': 1, 'error': 0}, room=sid, namespace='/')
+        socketio.sleep(0)
+    except:
         return
     print("hmm")
 
@@ -49,20 +82,26 @@ def ssh_login(info):
                     x = u(session.recv(1024))
                     if len(x) == 0:
                         break
-                    emit('response', {'data': x}, room=sid, namespace='/')
+                    emit('response', {'data': x, 'clear': 0, 'error': 0}, room=sid, namespace='/')
                     socketio.sleep(0)
                 except socket.timeout:
                     pass
-    sid = request.sid
-    hostname = info['hostname']
-    username = info['username']
-    password = info['password']
-    client = paramiko.Transport((hostname, 22))
-    client.connect(username=username, password=password)
-    session = client.open_channel(kind='session')
-    session.get_pty()
-    session.invoke_shell()
-    session.settimeout(0.00)
-    clients[sid] = (session)
-    thread = Thread(target = background_thread)
-    thread.start()
+    try:
+        sid = request.sid
+        hostname = info['hostname']
+        username = info['username']
+        password = info['password']
+        cols = int(info['cols'])
+        rows = int(info['rows'])
+        client = paramiko.Transport((hostname, 22))
+        client.connect(username=username, password=password)
+        session = client.open_channel(kind='session')
+        session.get_pty(width = cols, height = rows)
+        session.invoke_shell()
+        session.settimeout(0.00)
+        clients[sid] = (session, [cols, rows])
+        thread = Thread(target = background_thread)
+        thread.start()
+    except Exception as e:
+        emit('response', {'clear': 0, 'error': 1, 'err_msg': "\r\n" + str(e)}, room=sid, namespace='/')
+        socketio.sleep(0)
